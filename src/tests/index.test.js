17939a9a84cb540263736ae7f90521e24b052b37
alexander janet <ajt@dev-04.kopaxgroup>
2018-06-12 12:01:46 +1200

    first commit

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,15 +1,7 @@
-/**
- * Testing example
- */
+const bootstrapStyled = require('../index');
 
-import React from 'react';
-import { shallow } from 'enzyme';
-import ReactDemo from '../index';
-
-describe('<ReactDemo />', () => {
-  it('should render a <Dropdown /> tag', () => {
-    const renderedComponent = shallow(<ReactDemo />);
-    expect(renderedComponent.find('Dropdown').length).toBe(1);
+describe('api exports', () => {
+  it('should have all exports defined', () => {
+    Object.keys(bootstrapStyled).forEach((exported) => { expect(exported).toBeDefined(); });
   });
 });
-
